697959bde3099b16029dd5cdd5cab6d801b38dfb
CarlosStrider120 <sherifinho.augusto@gmail.com>
2022-06-14 17:27:28 -0300

    added tasks in localstorage

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-window.addEventListener('load', () => {
-	const newTaskForm = document.querySelector('#new-task-form');
-	const newTaskInput = document.querySelector('#new-task-input');
-	const taskList = document.querySelector('#tasks');
-	
-	newTaskForm.addEventListener('submit', (e) => {
-		e.preventDefault();
-		if (newTaskInput.value.trim() === '') {
-			return;
-		}
+let storageArray = [];
+const getStorage = () => JSON.parse(localStorage.getItem('todoList')) ?? [];
+const setStorage = (value) => localStorage.setItem('todoList', JSON.stringify(value));
+
+const newTaskForm = document.querySelector('#new-task-form');
+const newTaskInput = document.querySelector('#new-task-input');
+const taskList = document.querySelector('#tasks');
+
+const addTasks = () => {
+	for (let i = 0; i < getStorage().length; i++) {
 		const task = document.createElement('li');
 		const inputText = document.createElement('input');
 		const buttons = document.createElement('div');
@@ -22,15 +22,19 @@ window.addEventListener('load', () => {
 		buttonDelete.textContent = 'Delete';
 		inputText.type = 'text';
 		inputText.readOnly = true;
-		inputText.value = newTaskInput.value;
+		inputText.value = getStorage()[i].task;
 		task.appendChild(inputText);
 		buttons.appendChild(buttonEdit);
 		buttons.appendChild(buttonDelete);
 		task.appendChild(buttons);
 		taskList.appendChild(task);
-		newTaskInput.value = '';
-		
+
 		let isEditing = false
+		inputText.addEventListener('click', () => {
+			if (!isEditing) {
+				inputText.classList.toggle('done');
+			}
+		});
 		buttonEdit.addEventListener('click', () => {
 			if (!isEditing) {
 				inputText.readOnly = false;
@@ -39,13 +43,39 @@ window.addEventListener('load', () => {
 				isEditing = true;
 			}else {
 				inputText.readOnly = true;
+				storageArray = getStorage();
+				storageArray[i].task = inputText.value;
+				setStorage(storageArray);
 				buttonEdit.textContent = 'Edit';
 				isEditing = false;
 			}
 		});
-		
 		buttonDelete.addEventListener('click', () => {
 			taskList.removeChild(task);
+			storageArray = getStorage();
+			console.log(storageArray);
+			storageArray.splice(i, 1);
+			console.log(storageArray);
+			setStorage(storageArray);
 		});
-	});
+	}
+}
+
+const restartScreen = () => {
+	taskList.innerHTML = '';
+}
+
+newTaskForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	if (newTaskInput.value.trim() === '') {
+		return;
+	}
+	storageArray = getStorage();
+	storageArray.push({'task': newTaskInput.value, 'status': ''});
+	setStorage(storageArray);
+	restartScreen();
+	addTasks();
+	newTaskInput.value = '';
 });
+restartScreen();
+addTasks();
